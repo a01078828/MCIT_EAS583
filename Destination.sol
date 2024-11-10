a595524afcaa772a9dc385f62432955cc103eb49
a01078828 <61101399+a01078828@users.noreply.github.com>
2024-11-10 08:22:37 +0800

    Update Destination.sol

--- Destination.sol
+++ Destination.sol
@@ -24,7 +24,7 @@ contract Destination is AccessControl {
 
 	function wrap(address _underlying_token, address _recipient, uint256 _amount ) public onlyRole(WARDEN_ROLE) {
 		//YOUR CODE HERE
-    require(wrapped_tokens[_underlying_token] != address(0), "Token not supported");
+    		require(wrapped_tokens[_underlying_token] != address(0), "Token not supported");
 		address wrapped_token = wrapped_tokens[_underlying_token];
 		BridgeToken(wrapped_token).mint(_recipient, _amount);
 		emit Wrap(_underlying_token, wrapped_token, _recipient, _amount);
@@ -32,7 +32,7 @@ contract Destination is AccessControl {
 
 	function unwrap(address _wrapped_token, address _recipient, uint256 _amount ) public {
 		//YOUR CODE HERE
-    require(underlying_tokens[_wrapped_token] != address(0), "Token not supported");
+    		require(underlying_tokens[_wrapped_token] != address(0), "Token not supported");
 		address underlying_token = underlying_tokens[_wrapped_token];
 		BridgeToken(_wrapped_token).burnFrom(msg.sender, _amount);
 		emit Unwrap(underlying_token, _wrapped_token, msg.sender, _recipient, _amount);
@@ -40,7 +40,7 @@ contract Destination is AccessControl {
 
 	function createToken(address _underlying_token, string memory name, string memory symbol ) public onlyRole(CREATOR_ROLE) returns(address) {
 		//YOUR CODE HERE  
-    BridgeToken newToken = new BridgeToken(_underlying_token, name, symbol, address(this));
+    		BridgeToken newToken = new BridgeToken(_underlying_token, name, symbol, address(this));
 		address newTokenAddress = address(newToken);
 		underlying_tokens[newTokenAddress] = _underlying_token;
 		wrapped_tokens[_underlying_token] = newTokenAddress;
